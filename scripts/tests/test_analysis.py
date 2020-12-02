d1cdf5fb82d4545928fdb7ef250155fe956bc078
chris smith <smitty1e@gmail.com>
2020-12-01 21:49:23 -0500

    how to curve-fit the set?

--- scripts/tests/test_analysis.py
+++ scripts/tests/test_analysis.py
@@ -3,7 +3,8 @@ import itertools
 import pytest
 import matplotlib.pyplot as plt
 import numpy as np
-import sqlite3 #No value add for SQLAlchemy here
+from numpy.polynomial.polynomial import polyfit, polyval
+import sqlite3                                   #No value add for SQLAlchemy here
 
 # ALL_FIELDS
 #  results_fire_sim_id|run_number             |map_file           |people
@@ -95,7 +96,10 @@ def test_one_run_filter(conn,sql_runs2):
         arr.dtype.names = ['step', 'count_turtles', 'mean_escape_time']
         assert len(arr) == 143
 
+@pytest.mark.skip
 def test_one_run_plot(conn,sql_runs2):
+    '''Query for the first run. Polyfit the data. Plot the data, with the polyfit overlaid.
+    '''
     out_dir ='../docs/figures/'
     fig, ax = plt.subplots()
     crit = ' AND run_number=1;'
@@ -104,5 +108,36 @@ def test_one_run_plot(conn,sql_runs2):
         y = cur.execute(sql_runs2[0].replace(';',crit)).fetchall()
         arr = np.fromiter(y, dtype='i4,i4,f4')
         arr.dtype.names = ['step', 'count_turtles', 'mean_escape_time']
+        coeff = polyfit(arr['step'],arr['count_turtles'],3)
+        print(f'{coeff=}') 
         ax.plot(arr['step'],arr['count_turtles'])
+        ax.plot(arr['step'],polyval(arr['step'],coeff))
         plt.savefig(f'{out_dir}scrap.png')
+
+
+def test_one_run_full_map(conn,sql_runs2):
+    '''Query for the first run. Polyfit the data. Plot the data, with the polyfit overlaid.
+    '''
+    out_dir ='../docs/figures/'
+    fig, ax = plt.subplots()
+    a0=a1=a2=a3=[]
+    for i in range(1,31):
+        crit = f' AND run_number={i};'
+        with conn:
+            cur = conn.cursor()
+            y = cur.execute(sql_runs2[0].replace(';',crit)).fetchall()
+            arr = np.fromiter(y, dtype='i4,i4,f4')
+            arr.dtype.names = ['step', 'count_turtles', 'mean_escape_time']
+            coeff = polyfit(arr['step'],arr['count_turtles'],0)
+            a0.append(coeff[0])
+           #a1.append(coeff[1])
+           #a2.append(coeff[2])
+           #a3.append(coeff[3])
+            ax.plot(arr['step'],arr['count_turtles'])
+
+    a4=[np.average(a0)]#,np.average(a1),np.average(a2),np.average(a3)]
+    ax.plot(arr['step'],polyval(arr['step'],a4))
+    ax.set_ylabel('people')
+    ax.set_xlabel('tick')
+    ax.set_title(f'a.map, all runs')
+    plt.savefig(f'{out_dir}scrap_{i}.png')
