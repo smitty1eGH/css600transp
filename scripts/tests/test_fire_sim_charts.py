b6cfd75c0b173f8be9a802f4f1e852ce7cc83c55
chris smith <smitty1e@gmail.com>
2020-12-05 18:15:18 -0500

    adding candlestic_data.txt

--- scripts/tests/test_fire_sim_charts.py
+++ scripts/tests/test_fire_sim_charts.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
+from math import sqrt
+
 import sqlite3                                   #No value add for SQLAlchemy here
 
 import matplotlib.pyplot as plt
+import mplfinance as mpf
+import pandas as pd
 import pytest
-import numpy as np
 
 
 @pytest.fixture
@@ -67,24 +71,44 @@ def test_do_charts0(conn,sql_stats,filter_bits):
          [ ('exit_dims_2_a.map', 472.0, 491.6, 516.0, 120.4888888888888)
          , ('exit_dims_2_b.map', 474.0, 496.5, 522.0, 184.94444444444443)
          , ('exit_dims_2_c.map', 473.0, 496.0, 524.0, 213.33333333333334)]
-    '''
-    out_dir ='../docs/figures/'
 
+    To get the data into ohlc format for the candlestic:
+
+    open  = avg - sqrt(var)
+    high  = max
+    low   = min
+    close = avg + sqrt(var)
+
+    '''
+    out_dir = '../docs/figures/'
+
+    out = open('candlestic_data.txt','w')
     with conn:
         cur = conn.cursor()
         rows = cur.execute(sql_stats).fetchall()
         for i in filter_bits:
             for j in i[1]:
-                range_=[]
-                domain=[]
+                data=[]
+                labels=[]
                 l = [m for m in rows if m[0].startswith( i[0] % j )]
-                for n in l:
-                    range_.append(n[2])
-                    domain.append(n[0][:-4][-1:]) #just the letter the right of the period
-                #print(f'{range_=}\n{domain=}')
-                fig, ax = plt.subplots()
-                ax.scatter(domain,range_)
-                ax.set_ylabel('average escape steps')
-                ax.set_xlabel('map letter')
-                ax.set_title(f'map {i[0] % j}')
-                plt.savefig(f'{out_dir}chart_{i[0] % j}.png')
+                for d,n in enumerate(l):
+                    labels.append(n[0][:-4][-1:]) #letter
+                    data.append([datetime.now() + timedelta(days=d)
+                                ,n[2]-sqrt(n[4])
+                                ,n[3]
+                                ,n[1]
+                                ,n[2]+sqrt(n[4])])
+                out.write(str(data))
+                out.write('\n--\n')
+                #print(f'{data=}')
+               #pda = pd.DataFrame.from_records(data,index='Date',columns=['Date','Open','High','Low','Close'])
+               #pda.index.name = 'letter'
+               #pda.shape
+               #print(f'{pda=}')
+    out.close()
+
+         #      ax.scatter(domain,range_)
+         #   ax.set_ylabel('average escape steps')
+         #   ax.set_xlabel('map letter')
+         #   ax.set_title(f'map {i[0] % j}')
+         #   plt.savefig(f'{out_dir}chart_{i[0] % j}.png')
