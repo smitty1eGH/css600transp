71ea08163e54e53774bb9a7f29287a27338c6031
chris smith <smitty1e@gmail.com>
2020-12-09 23:07:27 -0500

    work in progress

---
+++ docs/auto/team_ten_final_report.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "team_ten_final_report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "letterpaper")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "hyperref"
+    "amsmath"
+    "listings"
+    "graphicx")
+   (LaTeX-add-bibliographies
+    "css600"))
+ :latex)
+
---
+++ docs/publish.sh
@@ -0,0 +1,5 @@
+pdflatex team_ten_final_report.tex
+bibtex team_ten_final_report.aux
+pdflatex team_ten_final_report.tex
+pdflatex team_ten_final_report.tex
+
